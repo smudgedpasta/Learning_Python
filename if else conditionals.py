828f2d7e379346b52426f953c248e7e979be6c1c
smudgedpasta <68189588+smudgedpasta@users.noreply.github.com>
2020-07-17 00:53:42 +0100

    Added example made for ternary operators

--- if else conditionals.py
+++ if else conditionals.py
@@ -33,3 +33,9 @@ if num > 100:
     print("You have entered a huge, three or more digit number!")
 else:
     print("You have entered a small, 1 or two digit number!")
+    
+# Short hand if else notation (ternary operators) is writing the code in condensed, single lines.
+
+a, b = 10^35, 1.67262
+c = a-b if a>b else b-a
+print(f"The difference between a proton's pressure and a proton's weight is {c}")
