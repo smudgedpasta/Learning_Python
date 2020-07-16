4736a668a9051a53886aacb420cb7f73430e1b3c
smudgedpasta <68189588+smudgedpasta@users.noreply.github.com>
2020-07-16 20:21:37 +0100

    Added references of printing increasing sizes of asterisks per line

--- loops.py
+++ loops.py
@@ -53,3 +53,13 @@ index = 0
 while index<6:
     print(index)
     index += 1
+    
+NumberOfLines_Input = int(input("Please enter the number of lines you wish to count to:\n"))
+for a in range(NumberOfLines_Input):
+    print(a+1)
+
+for b in range(1, int(input("Enter a number\n",))+1):print(b*"*")
+
+c = int(input("What size shall your pyramid be?\n"))
+for d in range(c):
+    print(" " * (c-d-1) + " *" * (d+1))
