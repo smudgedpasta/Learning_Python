aa714eb42d474ba384157d55451fa6630216a23a
smudgedpasta <68189588+smudgedpasta@users.noreply.github.com>
2020-07-19 13:36:26 +0100

    Added various examples of escape sequences

--- main.py
+++ main.py
@@ -846,3 +846,34 @@ block 5 - return the result
 
 # globals().clear()
 print(" ")
+
+# Main escape sequences:
+# \t = tab
+# \n = new line (linefeed)
+# \' = single quote
+# \" = double quote
+# \\ = prints backslash
+# \a = bell alert sound
+# \b = removes previous character
+# \f = formfeed
+# \N{name} = prints a unicode character
+
+# print('My name's Smudge') < This will error because of the ' in "name's"
+
+print("My name's Smudge") # < This works because the " and ' are different
+
+print('My name\'s Smudge')
+
+print("\\My name's Smudge\\") # < Two backslashes are interpreted as one
+
+print("My name's Smudge.\t Tabs are a thing I guess. :P")
+
+print("Test\a") # ¯\_(ツ)_/¯
+
+print("ab" + "\b" + "c")
+
+print("My name\fis Smudge")
+
+print(u'\N{DAGGER}')
+
+print(" ")
