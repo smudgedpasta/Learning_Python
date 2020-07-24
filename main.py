ba7dc3fa1c98a96967071625ad78cab35cb6db25
smudgedpasta <68189588+smudgedpasta@users.noreply.github.com>
2020-07-24 21:11:11 +0100

    Looked a small bit at classes/object oriented programming

--- main.py
+++ main.py
@@ -1079,3 +1079,23 @@ print(q)
 
 globals().clear()
 print(" ")
+
+'''
+This is "Object Oreiented Programming"
+In object oriented programming, you can model anything in your database to real world scenario. With:
+- Class
+- Attributes
+- Methods
+'''
+
+class Sonic:
+    Rings = 10
+    Name = "Sonic"
+    def RingCount(self):
+        return self.Rings
+R1 = Sonic() # < Creating a class object called R1
+R1.Rings = 342 # < If I comment this, the output is the default Ring Count (10)
+print(R1.RingCount()) # < Can also run print(R1.Rings())
+
+globals().clear()
+print(" ")
