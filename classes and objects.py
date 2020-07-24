5894ba5c2282b6896bd200bf374f149e2f0901bc
smudgedpasta <68189588+smudgedpasta@users.noreply.github.com>
2020-07-24 21:12:16 +0100

    Added classes and objects file

---
+++ classes and objects.py
@@ -0,0 +1,16 @@
+'''
+This is "Object Oreiented Programming"
+In object oriented programming, you can model anything in your database to real world scenario. With:
+- Class
+- Attributes
+- Methods
+'''
+
+class Sonic:
+    Rings = 10
+    Name = "Sonic"
+    def RingCount(self):
+        return self.Rings
+R1 = Sonic() # < Creating a class object called R1
+R1.Rings = 342 # < If I comment this, the output is the default Ring Count (10)
+print(R1.RingCount()) # < Can also run print(R1.Rings())
